d8b1e5f97639d916aa078d2f2d1611337d35b834
Yazky Maulana <yazki.maulana@gmail.com>
2025-09-16 16:50:31 +0700

    feat add: add deposit, numberOfLines, and getBet function

---
+++ project.js
@@ -0,0 +1,53 @@
+// 1. Deposite some money ~
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. Play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while (true) {
+        const depositAmount = prompt("Enter a deposit amount: ");
+        const numberDepositAmount = parseFloat(depositAmount);
+        
+        if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+            console.log('Invalid deposit amount, try again.');
+        } else {
+            return numberDepositAmount;
+        }
+    }
+};
+
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log('Invalid number of lines, try again.');
+        } else {
+            return numberOfLines;
+        }
+    }
+}
+
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the total bet: ");
+        const numberBet = parseFloat(bet);
+        
+        if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log('Invalid number of lines, try again.');
+        } else {
+            return numberBet;
+        }
+    }
+}
+
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
